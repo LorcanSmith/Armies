8a7a7a3395a027685d9c9e16d77b7c4859cf847b
will-ireland <will_ireland@outlook.com>
2025-02-27 04:29:54 +1100

    created basic scripts for combat and game managers

--- Scripts/Managers/battle_manager.gd
+++ Scripts/Managers/battle_manager.gd
@@ -1,5 +1,30 @@
 extends Node
 
+var battle_over = false
+
 ##TODO
 #SPAWN ARMIES
 #SWITCH BETWEEN SKILL/MOVING
+
+func _ready():
+#	Read in both armies and place them on the grid
+#	begin with move phase
+	battle_ticker()
+	
+func battle_ticker():
+	while !battle_over:
+		movement_phase()
+		combat_phase()
+
+func movement_phase():
+#	calls move() on all units
+#	if an enemy unit is 1 space away, don't move
+#	opposing units that occupy the same space will brawl
+	pass
+
+func combat_phase():
+#	call skill() for each unit that can use them or calculate brawl damage
+#	manage stats of both armies and both bases
+	var base_destroyed = true
+	if base_destroyed:
+		battle_over = true
---
+++ Scripts/Managers/game_manager.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+# swaps between the battle and shop scene
+@export var BattleManager : Node2D
+@export var ShopManager : Node2D
+
+func enter_shop():
+	pass
+	
+func enter_combat():
+	pass
