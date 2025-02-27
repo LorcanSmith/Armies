d610255e7df2f2cd61c02e21608740b32b6ed7e0
will-ireland <will_ireland@outlook.com>
2025-02-27 16:24:02 +1100

    updated gitignore to block .tmp files, renamed battle_manager to combat_manager, and updated game_manager to work with test shop scene

--- Scripts/Managers/game_manager.gd
+++ Scripts/Managers/game_manager.gd
@@ -1,11 +1,24 @@
 extends Node2D
 
 # swaps between the battle and shop scene
-@export var BattleManager : Node2D
+@export var CombatManager : Node2D
 @export var ShopManager : Node2D
+var current_scene : Node2D
+
+var army : Array
+
+var shop_scene = preload("res://Scenes/Test Scenes/Shop.tscn")
+
+func _ready():
+	enter_shop()
+	
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		enter_combat()
 
 func enter_shop():
-	pass
+	current_scene = shop_scene.instantiate()
+	add_child(current_scene)
 	
 func enter_combat():
-	pass
+	print("moved to combat")
