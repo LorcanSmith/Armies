a28abe491af0eead24c5947035bf96758beae9fe
will-ireland <will_ireland@outlook.com>
2025-03-03 01:18:53 +1100

    added a label that updates when the headquarters takes damage

--- Scripts/Managers/headquater.gd
+++ Scripts/Managers/headquater.gd
@@ -1,13 +1,21 @@
 extends Node
 
+@export var max_health = 150
 @export var health = 150
+
+func _ready():
+	update_label()
 
 #Does damage and heals headquaters
 #Positive numbers to hurt, negative numbers to heal
 func hurt(amount : int):
 	health -= amount
+	update_label()
 	if(health <= 0):
 		destroy_headquarters()
+		
+func update_label():
+	$Label.text = str(health) + "/" + str(max_health)
 
 #Called when the headquater runs out of head	
 func destroy_headquarters():
