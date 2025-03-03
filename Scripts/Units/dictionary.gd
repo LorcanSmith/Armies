7a163add7e855d271e32ad13ee2bd2d734af3eae
will-ireland <will_ireland@outlook.com>
2025-03-04 00:12:24 +1100

    updated dictionary to reflect id change for wizard unit

--- Scripts/Units/dictionary.gd
+++ Scripts/Units/dictionary.gd
@@ -4,7 +4,7 @@ extends Node
 # Dictionary to store unit IDs and their corresponding scene paths
 var unit_scenes = {
 	0: preload("res://Prefabs/Units/Level1/knight_unit.tscn"),
-	1: preload("res://Prefabs/Units/Level1/wizard_unit.tscn")
+	3: preload("res://Prefabs/Units/Level1/wizard_unit.tscn")
 }
 
 var item_scenes = {
