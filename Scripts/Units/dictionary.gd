a6877272fef47047d24911cdee4538f001235fce
LorcanSmith <lorcanwsmith@gmail.com>
2025-03-03 18:56:20 +0200

    knight unit updated

--- Scripts/Units/dictionary.gd
+++ Scripts/Units/dictionary.gd
@@ -4,8 +4,9 @@ extends Node
 # Dictionary to store unit IDs and their corresponding scene paths
 var unit_scenes = {
 	#KNIGHT
-	0: preload("res://Prefabs/Units/Level1/knight_unit.tscn"),
-	1: preload("res://Prefabs/Units/Level1/wizard_unit.tscn"),
+	0: preload("res://Prefabs/Units/Level1/knight_unit_LVL1.tscn"),
+	1: preload("res://Prefabs/Units/Level2/knight_unit_LVL2.tscn"),
+	2: preload("res://Prefabs/Units/Level3/knight_unit_LVL3.tscn"),
 	#ARCHER
 	3: preload("res://Prefabs/Units/Level1/archer_unit_LVL1.tscn"),
 	4: preload("res://Prefabs/Units/Level2/archer_unit_LVL2.tscn"),
@@ -14,8 +15,9 @@ var unit_scenes = {
 
 var item_scenes = {
 	#KNIGHT
-	0: preload("res://Prefabs/Shop Items/Level1/knight_item.tscn"),
-	1: preload("res://Prefabs/Shop Items/Level2/knight_level2_item.tscn"),
+	0: preload("res://Prefabs/Shop Items/Level1/knight_item_LVL1.tscn"),
+	1: preload("res://Prefabs/Shop Items/Level2/knight_item_LVL2.tscn"),
+	2: preload("res://Prefabs/Shop Items/Level3/knight_item_LVL3.tscn"),
 	#ARCHER
 	3: preload("res://Prefabs/Shop Items/Level1/archer_item_LVL1.tscn"),
 	4: preload("res://Prefabs/Shop Items/Level2/archer_item_LVL2.tscn"),
