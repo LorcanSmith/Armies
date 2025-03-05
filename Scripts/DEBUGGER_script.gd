dd499e98565085ead023e744a1b4209de6c66869
will-ireland <will_ireland@outlook.com>
2025-03-06 04:07:21 +1100

    script to create enemy armies now works dynamically for turn numbers
    script now creates an army for all 14 possible turns that can happen (the current turn limit with max losses and game victory)
    takes into account changing grid sizes and increasing budget

--- Scripts/DEBUGGER_script.gd
+++ Scripts/DEBUGGER_script.gd
@@ -36,71 +36,88 @@ func _input(event):
 						
 
 func create_enemy_armies():
-	var dictionary_instance = dictionary.new()
-	var money : int = 30
-	var threshold : int = 10
-#	item scenes
-	var items : Dictionary = dictionary_instance.item_scenes.duplicate()
-	var units : Array = []
-	var random_item : int
-	var price : int
 	var turn_number = 1
+	var dictionary_instance = dictionary.new()
+	var base_money : int = 30
+	var base_threshold : int = 10
+	var money : int
+	var threshold : int
+	var items : Dictionary
+	var units : Array
 	
+	var price : int
+	var random_item : int
 	var grid_width : int = 1
 	var grid_height : int = 1
-
 	var width_per_turn = [3,3,4,4]
 	var height_per_turn = [2,3,3,4]
 	
-	turn_number -= 1
-	if(width_per_turn[turn_number]):
-		grid_width = width_per_turn[turn_number]
-		grid_height = height_per_turn[turn_number]
-	else:
-		grid_width = width_per_turn[-1]
-		grid_height = height_per_turn[-1]
 	
-	while money > threshold and units.size() <= 6:
-		random_item = randi_range(1, items.size()) - 1
-		price = items[random_item].instantiate().buy_cost
+#	item scenes
+
+	while turn_number <= 14:
+	
+		units = []
 		
-		if price > money:
-			items.erase(random_item)
+		items = dictionary_instance.item_scenes.duplicate()
+		
+		money = base_money * turn_number
+		threshold = base_threshold * turn_number
+
+		
+		if(width_per_turn.size() > turn_number):
+			grid_width = width_per_turn[turn_number - 1]
+			grid_height = height_per_turn[turn_number - 1]
 		else:
-			money -= price
-			units.append([random_item, "enemy"])
-	while units.size() < 6:
-		units.append(null)
-	
-	units.shuffle()
-	var grid_data = []
-	print(units.slice(0, 2))
-	print(units.slice(2, 4))
-	print(units.slice(4, 6))
-	grid_data.append(units.slice(0, 2))
-	grid_data.append(units.slice(2, 4))
-	grid_data.append(units.slice(4, 6))
-	print(grid_data)
-	var game_folder = ProjectSettings.globalize_path("res://")
-	
-	var	grid_name = "enemy"
-	
-	var current_turn_number = "turn1"
-	
-	var json_string
-	
-	var file_path = game_folder + grid_name + current_turn_number + ".save"
-	var save_file
-	if FileAccess.file_exists(file_path):
-		#This will give you the project directory.
-		save_file = FileAccess.open(file_path, FileAccess.READ_WRITE)
-		# Move the file cursor to the end of the file (just in case it's not already there)
-		save_file.seek_end()
-	else:
-		save_file = FileAccess.open(file_path, FileAccess.WRITE)
-	# JSON provides a static method to serialize the grid_data to a string
-	json_string = JSON.stringify(grid_data)
-	#print(json_string)
-	# Store the save data as a new line in the save file
-	save_file.store_line(json_string)
-	save_file.close()  # Don't forget to close the file after you're done.
+			grid_width = width_per_turn[-1]
+			grid_height = height_per_turn[-1]
+		
+		var total_units : int = grid_width * grid_height
+		
+		while money > threshold and units.size() <= total_units:
+			random_item = randi_range(1, items.size()) - 1
+			if items.has(random_item):
+				price = items[random_item].instantiate().buy_cost
+				
+				if price > money:
+					items.erase(random_item)
+				else:
+					money -= price
+					units.append([random_item, "enemy"])
+		while units.size() < total_units:
+			units.append(null)
+		
+		units.shuffle()
+		var grid_data = []
+		var counter = 0
+		while counter < total_units:
+			#print(units.slice(0, 2))
+			#print(units.slice(2, 4))
+			#print(units.slice(4, 6))
+			grid_data.append(units.slice(counter, counter + grid_height))
+			counter += grid_height
+		var game_folder = ProjectSettings.globalize_path("res://")
+		
+		var	grid_name = "enemy"
+		
+		var current_turn_number = "turn" + str(turn_number)
+		
+		var json_string
+		
+		var file_path = game_folder + grid_name + current_turn_number + ".save"
+		var save_file
+		if FileAccess.file_exists(file_path):
+			#This will give you the project directory.
+			save_file = FileAccess.open(file_path, FileAccess.READ_WRITE)
+			# Move the file cursor to the end of the file (just in case it's not already there)
+			save_file.seek_end()
+		else:
+			save_file = FileAccess.open(file_path, FileAccess.WRITE)
+		# JSON provides a static method to serialize the grid_data to a string
+		json_string = JSON.stringify(grid_data)
+		#print(json_string)
+		# Store the save data as a new line in the save file
+		save_file.store_line(json_string)
+		save_file.close()  # Don't forget to close the file after you're done.
+		
+		turn_number += 1
