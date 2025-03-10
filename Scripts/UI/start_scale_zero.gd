920b48c31fdf41fffee8d6c940a50537967d1aa1
LorcanSmith <lorcanwsmith@gmail.com>
2025-03-10 13:38:41 +0200

    Tooltip visuals updated

---
+++ Scripts/UI/start_scale_zero.gd
@@ -0,0 +1,4 @@
+extends Node
+
+func _ready() -> void:
+	self.scale = Vector2i(0,0)
--- Scripts/UI/tooltip.gd
+++ Scripts/UI/tooltip.gd
@@ -2,9 +2,7 @@ extends Control
 #Unit dictionary, used to spawn in upgraded units
 var dictionary = preload("res://Scripts/Units/dictionary.gd")
 
-@export var image_used : bool
-@export var image : Sprite2D
-
+var unit_name : RichTextLabel
 var description : RichTextLabel
 var health : RichTextLabel
 var skill_damage : RichTextLabel
@@ -17,15 +15,17 @@ func update_tooltip() -> void:
 		var dictionary_instance = dictionary.new()
 		var unit = dictionary_instance.unit_scenes[get_parent().unit_ID].instantiate()
 		var item = dictionary_instance.item_scenes[get_parent().unit_ID].instantiate()
+		unit_name = find_child("name")
 		description = find_child("description")
 		health = find_child("health")
 		skill_damage = find_child("skill")
 		skill_heal = find_child("heal")
 		brawl = find_child("brawl")
 		cost = find_child("cost")
+		unit_name.text = unit.name
 		description.text = item.description
 		health.text = str("Health: ", unit.max_health)
 		skill_damage.text = str("Skill Damage: ", unit.skill_damage)
 		skill_heal.text = str("Skill Heal: ", unit.skill_heal)
 		brawl.text = str("Brawl Damage: ", unit.brawl_damage)
-		cost.text = str("Buy Cost: ", item.buy_cost, "Sell Cost: ", item.sell_cost)
+		cost.text = str("Buy Cost: ", item.buy_cost, " / Sell Cost: ", item.sell_cost)
