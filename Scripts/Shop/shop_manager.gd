d0a39158b0363f5974f1f55200b8b840cda36283
LorcanSmith <lorcanwsmith@gmail.com>
2025-03-11 14:41:44 +0200

    Added an icon to show when units can be upgraded. Units can only be upgraded if they are the same level

--- Scripts/Shop/shop_manager.gd
+++ Scripts/Shop/shop_manager.gd
@@ -9,6 +9,7 @@ var status_bar: StatusBar
 
 #Amount of money the player can spend each turn
 @export var money : int = 30
+var same_units = []
 
 func _ready() -> void:
 	game_manager = find_parent("game_manager")
@@ -22,3 +23,26 @@ func change_money(amount : int):
 
 func _on_battle_button_pressed() -> void:
 	game_manager.swap_scenes()
+
+func show_potential_upgrades(show : bool, item_who_called : Node2D):
+	if(show):
+		var tiles = find_child("grid_manager").find_child("grid_generator (army)").get_children()
+		var x = 0
+		#Loops over all tiles
+		while (x < (tiles.size())):
+			var y = 0
+			var children = tiles[x].get_children()
+			while (y < (children.size())):
+				#If there is a item on the tile
+				if(children[y].is_in_group("item")):
+					#if that item is the same as the current item and it can be upgraded
+					if(children[y].unit_ID == item_who_called.unit_ID and children[y].can_be_upgraded and children[y] != item_who_called):
+						#Add it to the same units array
+						same_units.append(children[y])
+				y+=1
+			x+=1
+	for unit in same_units:
+		if(unit):
+			unit.upgrade_arrow.visible = show
+	if(!show):
+		same_units = []
