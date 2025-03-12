6a0521fd0312db7bbbc484caff35b859eb483c85
LorcanSmith <lorcanwsmith@gmail.com>
2025-03-12 12:45:08 +0200

    Skill holder now waits to say it has no children. Thus waiting for all skils/projectiles to spawn in

--- Scripts/Grid/tile.gd
+++ Scripts/Grid/tile.gd
@@ -6,9 +6,9 @@ var is_empty : bool = true
 var units_on_tile : Array = []
 
 func unit_placed_on(unit):
-	is_empty = false
 	if(!units_on_tile.has(unit)):
 		units_on_tile.append(unit)
+	is_empty = false
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
--- Scripts/Skills/skill_holder.gd
+++ Scripts/Skills/skill_holder.gd
@@ -6,11 +6,15 @@ extends Node
 #Are we waiting for skills to finish
 var waiting_for_skills : bool = false
 
+#Delays are so they skill holder doesnt say its empty whilst skills are still being spawned in
+var delay = 0.3
+var current_delay = 0.3
 func _process(delta: float) -> void:
-	print(get_children())
 	if(waiting_for_skills):
+		current_delay -= delta
 		#If we have no children it means there are no more skills to wait for
-		if(self.get_child_count() == 0):
+		if(current_delay <= 0 and self.get_child_count() == 0):
 			waiting_for_skills = false
+			current_delay = delay
 			#Tell the combat manager to proceed with combat
 			get_parent().no_skills_left()
