4053eec4fb4b6167ef2a170023c6f49e9d657889
LorcanSmith <lorcanwsmith@gmail.com>
2025-03-26 00:12:02 +0200

    Tooltip flips position if off the edge of the screen, also added popin and popout animations for tooltips

--- Scripts/Skills/buff_visual.gd
+++ Scripts/Skills/buff_visual.gd
@@ -1,5 +1,6 @@
 extends Node
 
+var unit : Node2D
 var target : Node2D
 var move : bool = false
 var starting_location : Vector2
@@ -10,19 +11,31 @@ var t = 0
 func _process(delta: float) -> void:
 	if(move):
 		t += delta
-		self.global_position = lerp(starting_location, target.global_position, t*3)
-		if(self.global_position.distance_to(target.global_position) < 1):
+		#If this is a health buff, set the target to the heart sprite
+		if(this_is_health_buff):
+			target = unit.find_child("heart_sprite")
+		#If this is a damage buff, set the target to the sword sprite
+		else:
+			target = unit.find_child("sword_sprite")
+		#Move the heart/sword sprite towards the target
+		find_child("Sprite2D").global_position = lerp(starting_location, target.global_position, t*3)
+		#If the sprite is close enough to the target location
+		if(find_child("Sprite2D").global_position.distance_to(target.global_position) < 1):
+			#Tell it to stop moving
 			move = false
+			#Fade out
 			get_node("AnimationPlayer").play("fade_out")
+			#Make the heart/sword on the unit bounce a little for visual feedback
 			if(this_is_health_buff):
-				target.get_node("AnimationPlayer").play("health_bounce")
+				unit.get_node("AnimationPlayer").play("health_bounce")
 			else:
-				target.get_node("AnimationPlayer2").play("damage_bounce")
-
+				unit.get_node("AnimationPlayer2").play("damage_bounce")
+			#Update the text on the unit health/damage label
+			unit.update_label_text()
 
 func _on_animation_player_animation_finished(anim_name : String) -> void:
 	if(anim_name == "buff_appear"):
-		starting_location = self.global_position
+		starting_location = find_child("Sprite2D").global_position
 		move = true
 	if(anim_name == "fade_out"):
 		queue_free()
