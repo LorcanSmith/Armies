d2bd1bece254a0c9dace120ea46a68b73fc4bf93
LorcanSmith <lorcanwsmith@gmail.com>
2025-03-25 21:58:14 +0200

    New animations

--- Scripts/Skills/buff_visual.gd
+++ Scripts/Skills/buff_visual.gd
@@ -3,6 +3,8 @@ extends Node
 var target : Node2D
 var move : bool = false
 var starting_location : Vector2
+#Used to determine what animation to play: health or damage
+@export var this_is_health_buff : bool
 
 var t = 0
 func _process(delta: float) -> void:
@@ -12,6 +14,10 @@ func _process(delta: float) -> void:
 		if(self.global_position.distance_to(target.global_position) < 1):
 			move = false
 			get_node("AnimationPlayer").play("fade_out")
+			if(this_is_health_buff):
+				target.get_node("AnimationPlayer").play("health_bounce")
+			else:
+				target.get_node("AnimationPlayer2").play("damage_bounce")
 
 
 func _on_animation_player_animation_finished(anim_name : String) -> void:
