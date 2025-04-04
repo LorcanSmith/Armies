89864f5383a758bf462e649b24f0c76df020de10
LorcanSmith <lorcanwsmith@gmail.com>
2025-04-04 17:00:34 +0300

    Reloading isn't skipped if no units can attack

--- Scripts/Managers/combat_manager.gd
+++ Scripts/Managers/combat_manager.gd
@@ -150,7 +150,6 @@ func movement_phase():
 						if(unit.moved == false):
 							units_to_move.append(unit)
 							unit.moved = true
-						
 						#Add this unit to the array as its still alive
 						if(!player_army.has(unit)):
 							player_army.append(unit) 
@@ -205,12 +204,12 @@ func waited_for_move():
 		var u = 0
 		combat_ready_unit_alive = false
 		while u < player_army.size():
-			if(player_army[u].enemies_in_range.size() > 0 or player_army[u].get_parent().units_on_tile.size() > 1):
+			if(player_army[u].enemies_in_range.size() > 0 or player_army[u].get_parent().units_on_tile.size() > 1 or player_army[u].reloading):
 				combat_ready_unit_alive = true
 			u += 1
 		u = 0
 		while u < enemy_army.size():
-			if(enemy_army[u].enemies_in_range.size() > 0 or enemy_army[u].get_parent().units_on_tile.size() > 1):
+			if(enemy_army[u].enemies_in_range.size() > 0 or enemy_army[u].get_parent().units_on_tile.size() > 1 or enemy_army[u].reloading):
 				combat_ready_unit_alive = true
 			u += 1
 		#Makes sure we don't do two sets of movement
